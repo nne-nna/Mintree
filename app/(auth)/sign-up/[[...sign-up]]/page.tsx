83ed9163d967ecc6c0a2b0d40c1cd6c34604b89c
Ezidiegwu Nnenna <ezidiegwunnennafavour@gmail.com>
2025-10-23 06:21:07 +0100

    feat: add Clerk authentication (sign-in & sign-up) and integrate Hono for API routing

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,30 @@
+import { ClerkLoaded, ClerkLoading, SignUp } from "@clerk/nextjs";
+import { HandCoins, Loader2 } from "lucide-react";
+
+const SignUpPage = () => {
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 pb-12">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome Back</h1>
+          <p className="text-base text-[#7E8CA0]">
+            Log in or create account to get back to your dashboard!
+          </p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignUp path="/sign-up" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+        <HandCoins className="h-[100px] w-[100px]" />
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
